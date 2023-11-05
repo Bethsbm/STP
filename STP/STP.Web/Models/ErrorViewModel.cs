620a051d892d18758b1a5afe105f953a9fc16ba8
bethsbm <maritza_sosa@live.com>
2023-11-05 16:23:36 -0600

    Sincronizando cambios

---
+++ STP/STP.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace STP.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
